79dac6cd2827f28947db1d01216bc41f8f6c1ce0
joao <sonicjoa06@gmail.com>
2026-06-11 16:23:10 -0300

    criando model do produto

---
+++ UltraZone1/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraZone1.Models
+{
+    public class Produto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
